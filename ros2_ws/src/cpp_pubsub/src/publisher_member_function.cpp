6df189d29381c4ea5458ef339209e973f01b3d15
sor-bai <sor-bai>
2026-02-08 05:53:25 -0800

    [ROS2] add ROS2 QoS

--- ros2_ws/src/cpp_pubsub/src/publisher_member_function.cpp
+++ ros2_ws/src/cpp_pubsub/src/publisher_member_function.cpp
@@ -14,43 +14,42 @@
 
 #include <chrono>
 #include <memory>
+#include <thread>
 
 #include "rclcpp/rclcpp.hpp"
+// string.msg-ros 序列化编译器，生成的头文件
 #include "std_msgs/msg/string.hpp"
 
 using namespace std::chrono_literals;
 
-/* This example creates a subclass of Node and uses std::bind() to register a
- * member function as a callback from the timer. */
-
-class MinimalPublisher : public rclcpp::Node
-{
-public:
-  MinimalPublisher()
-  : Node("minimal_publisher"), count_(0)
-  {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-    timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
-  }
-
-private:
-  void timer_callback()
-  {
-    auto message = std_msgs::msg::String();
-    message.data = "Hello, world! " + std::to_string(count_++);
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-    publisher_->publish(message);
-  }
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-  size_t count_;
-};
-
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  
+  auto node = rclcpp::Node::make_shared("minimal_publisher");
+
+  rclcpp::QoS qos_profile(1);
+  // 尽量而为
+  qos_profile.best_effort(); 
+  
+  auto publisher = node->create_publisher<std_msgs::msg::String>("topic", qos_profile);
+  
+  size_t count = 0;
+  
+  rclcpp::Rate rate(2); 
+  
+  while (rclcpp::ok()) {
+    auto message = std_msgs::msg::String();
+    message.data = "Hello, world! " + std::to_string(count++);
+    RCLCPP_INFO(node->get_logger(), "Publishing: '%s'", message.data.c_str());
+    publisher->publish(message);
+    
+    rclcpp::spin_some(node);
+    
+    rate.sleep();
+  }
+  
   rclcpp::shutdown();
   return 0;
 }
+
--- ros2_ws/src/cpp_pubsub/src/subscriber_member_function.cpp
+++ ros2_ws/src/cpp_pubsub/src/subscriber_member_function.cpp
@@ -16,30 +16,27 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
-using std::placeholders::_1;
 
-class MinimalSubscriber : public rclcpp::Node
+void topic_callback(const std_msgs::msg::String::SharedPtr msg)
 {
-public:
-  MinimalSubscriber()
-  : Node("minimal_subscriber")
-  {
-    subscription_ = this->create_subscription<std_msgs::msg::String>(
-      "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
-  }
-
-private:
-  void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
-  {
-    RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
-  }
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
-};
+  RCLCPP_INFO(rclcpp::get_logger("minimal_subscriber"), "I heard: '%s'", msg->data.c_str());
+}
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalSubscriber>());
-  rclcpp::shutdown();
-  return 0;
+  
+  auto node = rclcpp::Node::make_shared("minimal_subscriber");
+  
+  rclcpp::QoS qos_sub(10);
+  // qos_sub.reliable();
+  qos_sub.best_effort();
+
+  auto subscription = node->create_subscription<std_msgs::msg::String>(
+        "topic", qos_sub, topic_callback);
+
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
 }
+
